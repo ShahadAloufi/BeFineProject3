9716df6ea31c673545a16c541d63900808c12e76
shahdmefleh <69356275+shahdmefleh@users.noreply.github.com>
2022-05-28 20:29:46 +0300

    overrideUserInterfaceStyle

--- BeFine/SceneDelegate.swift
+++ BeFine/SceneDelegate.swift
@@ -16,6 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.rootViewController = MainTabBarViewController()
         window?.makeKeyAndVisible()
+        window?.overrideUserInterfaceStyle = .dark
        
     }
 
